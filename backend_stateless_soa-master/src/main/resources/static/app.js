4968c2c3b3c8e7ca89e5a1e0e176b405d92872fc
Santiago Ezequiel Sosa <santiago.ezequiel.sosa.arg@gmail.com>
2020-02-16 21:46:19 -0300

    Modificacion CSE y SAT

--- backend_stateless_soa-master/src/main/resources/static/app.js
+++
@@ -1,5 +0,0 @@
-angular.module('frontend',
-		['ngRoute','ngAnimate','ngTouch',
-		 'ui.bootstrap','ngStorage',
-		 'ngLoadingSpinner', 'ngStomp', 'chart.js']
-);
--- backend_stateless_soa-master/src/main/resources/static/controllers/div2.controller.js
+++
@@ -1,5 +0,0 @@
-app=angular.module('frontend');
-app.controller('div2Controller',function($scope){
-	$scope.titulo="Hola Mundo, desde controller 2";
-	
-});
